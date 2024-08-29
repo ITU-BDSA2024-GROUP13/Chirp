8f50e23c6ead5496d73514154d6a51f36637df23
André Birk <148399983+AndrxB@users.noreply.github.com>
2024-08-29 13:34:35 +0200

    Trying to upload the Chirp.CLI project

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,8 @@
+﻿DateTime currentTime = DateTime.UtcNow;
+long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+
+Console.WriteLine(unixTime);
+
+DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+DateTimeOffset.UtcNow.
