4a164b76336609add8bc5e4653ded4e550b1bd97
André <arbi@itu.dk>
2024-08-30 12:42:30 +0200

    trying to change to .net7 instead of .net8

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 ﻿DateTime currentTime = DateTime.UtcNow;
 long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+// is a method that converts the current time to Unix time.
+// Unix time is the number of seconds that have elapsed since 00:00:00 Coordinated Universal Time (UTC),
+// Thursday, 1 January 1970.
 
-Console.WriteLine(unixTime);
+Console.WriteLine("Unix time: " + unixTime);
+unixTime
+Console.WriteLine("Current time: " + currentTime);
 
-DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+// Console.WriteLine("User input: " + userInput);
 
