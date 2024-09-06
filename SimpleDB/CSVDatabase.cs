8bb69525107db37046e6871d27e140a6295fb6c1
polu <polu@itu.dk>
2024-09-06 08:43:54 +0200

    Added base structure of CSVDatabase class

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleDB;
+
+public class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    private readonly string _filePath;
+
+    public CSVDatabase(string filePath)
+    {
+        _filePath = filePath;
+    }
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Store(T record)
+    {
+        throw new NotImplementedException();
+    }
+}
