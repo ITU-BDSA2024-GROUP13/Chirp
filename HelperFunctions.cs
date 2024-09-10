1c8f4e7a3d8e569e42f9f3bfc2b8df4a123d6eb2
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-09-10 17:05:33 +0200

    Documentation for Helperfunction and IDatabaseRepo

--- HelperFunctions.cs
+++ HelperFunctions.cs
@@ -2,8 +2,19 @@ using System.Text.RegularExpressions;
 
 namespace Chirp.CLI;
 
+
+/**
+ * A list of functions which do not match an ideal responsible class
+ */
 public static class HelperFunctions
 {
+    /**
+     * <summary>
+     * Converting unix time from a long, into a date. <br />
+     * <example> 1725801466  ->  9/8/2024 </example>
+     * Note that the order is month/day/year.
+     * </summary>
+     */
     public static DateTime FromUnixTimeToDateTime(long unixTime)
     {
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -2,6 +2,10 @@ using System.Collections.Generic;
 
 namespace SimpleDB;
 
+/**
+ * Interface <c>IDatabaseRepository</c> helps ensure that, whatever implements it,
+ * can <c>Read()</c> and <c>Store()</c> information into a database.
+ */
 public interface IDatabaseRepository<T>
 {
     public List<T> Read(int? limit = null);
