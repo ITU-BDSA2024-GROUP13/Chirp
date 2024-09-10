4296532430b5cc665969d3ff69a9c2c6b9e00410
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-09-10 16:38:01 +0200

    full documentation on CSVDatabase

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
+﻿using System.Globalization;
 using CsvHelper;
 using CsvHelper.Configuration;
 
 namespace SimpleDB;
 
+/**
+ * <summary>
+ * class <c>CSVDatabase</c> is responsible for directly communicating with
+ * any stored data contained in a csv file.
+ * Additionally, this class implements the <c>IDatabaseRepository</c> interface
+ * </summary>
+ */
 public class CSVDatabase<T> : IDatabaseRepository<T>
 {
     private readonly string _filePath;
 
+    /**
+     * <summary>Constructor for <c>CSVDatabase</c>
+     * <param name="filePath">The csv-file to communicate with</param>
+     * </summary>
+     * 
+     */
     public CSVDatabase(string filePath)
     {
         _filePath = filePath;
     }
+    
+
+    /**
+     * <summary>
+     * <returns> A list of all records (limit for records is not implemented) </returns>
+     * </summary>
+     */
     public List<T> Read(int? limit = null)
     {
         using var reader = new StreamReader(_filePath);
@@ -22,6 +40,11 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
         return records;
     }
 
+    /**
+     * Stores a record or object, by writing into a csv file.
+     * The csvConfiguration is set with <c>CultureInfo.InvariantCulture</c>>
+     * in order to provide independency from the user's local settings.
+     */
     public void Store(T record)
     {
         var config = new CsvConfiguration(CultureInfo.InvariantCulture)
