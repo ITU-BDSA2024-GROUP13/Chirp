8db14b97e3e6f4a4f5b53c3b201ea8558db6a555
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-09-10 15:47:59 +0200

    Doc for UserInterface
    Methods:
    Chirp()
    PrintFromDatabaseConsole()

--- UserInterface.cs
+++ UserInterface.cs
@@ -2,8 +2,6 @@ using SimpleDB;
 
 namespace Chirp.CLI;
 using System;
-using DocoptNet;
-#nullable enable
 
 public static class UserInterface
 {
@@ -34,6 +32,11 @@ public static class UserInterface
         }
     }
     
+    /**
+     * <summary> This method iterates over an entire database for cheeps,
+     * and prints each cheep-object with their <c>ToString()</c> method.
+     * </summary>
+     */
     public static void PrintFromDatabaseToConsole<T>(IDatabaseRepository<T> database)
     {
         foreach (var cheep in database.Read())
@@ -41,13 +44,18 @@ public static class UserInterface
                 Console.WriteLine(cheep.ToString());
     }
     
+    /**
+     * <summary> This method creates a new Cheep-object, writes the result in the terminal,
+     * and stores the Cheep-object in a database.
+     * <param name="unixTime">Timestamp for Cheep-instantiation.</param>
+     * <param name="username">Author of the Cheep.</param>
+     * </summary>
+     */
     public static void Chirp(string username, string message, long unixTime, IDatabaseRepository<Cheep> database)
     { //Write message with relevant information
         Cheep cheep = new Cheep{Author = username, Message = message, Timestamp = unixTime};
         Console.WriteLine(cheep.ToString()); // may be deleted in the future
-        
         database.Store(cheep);
-    
     }
     
 }
