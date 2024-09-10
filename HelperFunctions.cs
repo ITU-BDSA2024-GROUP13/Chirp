efeee5de209f984513ac6af671c3ab8bc274de23
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-09-10 15:57:28 +0200

    Removing unused regex helper function
    ;(

--- HelperFunctions.cs
+++ HelperFunctions.cs
@@ -11,15 +11,4 @@ public static class HelperFunctions
         return dateTime;        
     }
     
-    public static string formatFromFileToConsole(string line)
-    {
-        var regex = new Regex(
-            "^(?<author>[æøåa-zÆØÅA-z0-9_-]*),[\"\"](?<message>.*)[\"\"],(?<timeStamp>[0-9]*)$");
-        var match = regex.Match(line);
-        var author = match.Groups["author"];
-        var message = match.Groups["message"];
-        var timestamp = HelperFunctions.FromUnixTimeToDateTime(int.Parse(match.Groups["timeStamp"].Value));
-    
-        return String.Format("{0} @ {1}: {2}", author, timestamp, message);
-    }
 }
