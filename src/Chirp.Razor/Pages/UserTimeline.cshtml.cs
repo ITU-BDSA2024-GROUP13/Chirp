b42dc08a204b11883ddbf7b346f62c9ae6eaf0dc
André Birk <arbi@itu.dk>
2024-09-25 21:03:36 +0200

    trying again

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -6,7 +6,7 @@ namespace Chirp.Razor.Pages;
 public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
-    public List<CheepViewModel> Cheeps { get; set; }
+    public required List<CheepViewModel> Cheeps { get; set; }
 
     public UserTimelineModel(ICheepService service)
     {
