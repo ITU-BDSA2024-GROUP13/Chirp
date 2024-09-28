438b0a86d6c4dcd86166ac179fa7b57540c7eab5
André Birk <arbi@itu.dk>
2024-09-28 16:29:28 +0200

    added docu for DBFacade

--- src/Chirp.CSVDBService/DBFacade.cs
+++ src/Chirp.CSVDBService/DBFacade.cs
@@ -1,141 +1,170 @@
-namespace Chirp.CSVDBService;
-
-using Chirp.CLI.Client;
-using System.Data;
-using Microsoft.Data.Sqlite;
-
-
-public class DBFacade
+namespace Chirp.CSVDBService
 {
-    string sqlDBFilePath = "../../data/chirps.db";
+    using Chirp.CLI.Client;
+    using System.Data;
+    using Microsoft.Data.Sqlite;
 
-    public DBFacade()
+    /** 
+     * <summary>
+     * Facade for database operations related to users and messages in the Chirp application.
+     * Provides methods to insert and select data from the database.
+     * </summary>
+     */ 
+    public class DBFacade
     {
-        SQLitePCL.Batteries.Init();
-    }
+        /** 
+         * <summary>
+         * File path to the SQLite database.
+         * </summary>
+         */ 
+        string sqlDBFilePath = "../../data/chirps.db";
 
-    public bool INSERT_USER(int user_id, string username, string email, string pw_hash)
-    {
-        
-        var sqlQuery = $"INSERT INTO user VALUES({user_id}, '{username}', '{email}', '{pw_hash}')";
-
-        try{
-            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-            {
-
-                
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                command.CommandText = sqlQuery;
-
-                using var reader = command.ExecuteReader();
-                return true;
-            }
-        } 
-        catch (Exception e)
+        /** <summary>
+         * Initializes a new instance of the DBFacade class and initializes SQLite batteries.
+         * </summary>
+         */ 
+        public DBFacade()
         {
-            Console.WriteLine(e.Message);
-            return false;
+            SQLitePCL.Batteries.Init();
         }
-    }
 
-    public bool INSERT_MESSAGE(int message_id, int auther_id, string text, int pub_date)
-    {
-        
-        var sqlQuery = $"INSERT INTO message VALUES({message_id}, {auther_id}, '{text}', {pub_date})";
+        /** 
+         * <summary>
+         * Inserts a new user record into the 'user' table.
+         * </summary>
+         * <param name="user_id">The ID of the user.</param>
+         * <param name="username">The username of the user.</param>
+         * <param name="email">The email address of the user.</param>
+         * <param name="pw_hash">The hashed password of the user.</param>
+         * <returns>Returns true if the insertion was successful, false otherwise.</returns>
+         */
+        public bool INSERT_USER(int user_id, string username, string email, string pw_hash)
+        {
+            var sqlQuery = $"INSERT INTO user VALUES({user_id}, '{username}', '{email}', '{pw_hash}')";
 
-        try{
-            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+            try
             {
-
-                
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                command.CommandText = sqlQuery;
-
-                using var reader = command.ExecuteReader();
-                return true;
+                using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = sqlQuery;
+                    using var reader = command.ExecuteReader();
+                    return true;
+                }
             }
-        } 
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            return false;
-        }
-    }
-
-    
-    public void SELECT_MESSAGE_FROM_USER(string username)
-    {
-        
-        var sqlQuery = $"SELECT U.username, M.text FROM user U, message M WHERE U.user_id = M.author_id AND U.username = '{username}';";
-
-        try {
-            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            catch (Exception e)
             {
-                // https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader?view=dotnet-plat-ext-7.0#examples
-                var dataRecord = (IDataRecord)reader;
-                for (int i = 0; i < dataRecord.FieldCount; i++)
-                    Console.WriteLine($"{dataRecord.GetName(i)}: {dataRecord[i]}");
-
-                // See https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader.getvalues?view=dotnet-plat-ext-7.0
-                // for documentation on how to retrieve complete columns from query results
-                Object[] values = new Object[reader.FieldCount];
-                int fieldCount = reader.GetValues(values);
-                for (int i = 0; i < fieldCount; i++)
-                    Console.WriteLine($"{reader.GetName(i)}: {values[i]}");
+                Console.WriteLine(e.Message);
+                return false;
             }
         }
-        }
-        catch (Exception e)
+
+        /** 
+         * <summary>
+         * Inserts a new message record into the 'message' table.
+         * </summary>
+         * <param name="message_id">The ID of the message.</param>
+         * <param name="author_id">The ID of the author (user) who created the message.</param>
+         * <param name="text">The text content of the message.</param>
+         * <param name="pub_date">The publication date of the message as Unix timestamp.</param>
+         * <returns>Returns true if the insertion was successful, false otherwise.</returns>
+         */
+        public bool INSERT_MESSAGE(int message_id, int author_id, string text, int pub_date)
         {
-            Console.WriteLine(e.Message);
-        }
-    }
+            var sqlQuery = $"INSERT INTO message VALUES({message_id}, {author_id}, '{text}', {pub_date})";
 
-    
-    public void ReadDatabase()
-    {
-        var sqlQuery = @"SELECT * FROM user";
-
-        try {
-            using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
-
-            var command = connection.CreateCommand();
-            command.CommandText = sqlQuery;
-
-            using var reader = command.ExecuteReader();
-            while (reader.Read())
+            try
             {
-                // https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader?view=dotnet-plat-ext-7.0#examples
-                var dataRecord = (IDataRecord)reader;
-                for (int i = 0; i < dataRecord.FieldCount; i++)
-                    Console.WriteLine($"{dataRecord.GetName(i)}: {dataRecord[i]}");
-
-                // See https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqldatareader.getvalues?view=dotnet-plat-ext-7.0
-                // for documentation on how to retrieve complete columns from query results
-                Object[] values = new Object[reader.FieldCount];
-                int fieldCount = reader.GetValues(values);
-                for (int i = 0; i < fieldCount; i++)
-                    Console.WriteLine($"{reader.GetName(i)}: {values[i]}");
+                using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = sqlQuery;
+                    using var reader = command.ExecuteReader();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
             }
         }
-        }
-        catch (Exception e)
+
+        /**
+         * <summary>
+         * Retrieves messages created by a specific user and prints the username and message text to the console.
+         * </summary>
+         * <param name="username">The username of the user whose messages you want to retrieve.</param>
+         */
+        public void SELECT_MESSAGE_FROM_USER(string username)
         {
-            Console.WriteLine(e.Message);
+            var sqlQuery = $"SELECT U.username, M.text FROM user U, message M WHERE U.user_id = M.author_id AND U.username = '{username}';";
+
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = sqlQuery;
+
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var dataRecord = (IDataRecord)reader;
+                        for (int i = 0; i < dataRecord.FieldCount; i++)
+                            Console.WriteLine($"{dataRecord.GetName(i)}: {dataRecord[i]}");
+
+                        Object[] values = new Object[reader.FieldCount];
+                        int fieldCount = reader.GetValues(values);
+                        for (int i = 0; i < fieldCount; i++)
+                            Console.WriteLine($"{reader.GetName(i)}: {values[i]}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /**
+         * <summary>
+         * Reads and prints all data from the 'user' table.
+         * </summary>
+         */
+        public void ReadDatabase()
+        {
+            var sqlQuery = @"SELECT * FROM user";
+
+            try
+            {
+                using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = sqlQuery;
+
+                    using var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var dataRecord = (IDataRecord)reader;
+                        for (int i = 0; i < dataRecord.FieldCount; i++)
+                            Console.WriteLine($"{dataRecord.GetName(i)}: {dataRecord[i]}");
+
+                        Object[] values = new Object[reader.FieldCount];
+                        int fieldCount = reader.GetValues(values);
+                        for (int i = 0; i < fieldCount; i++)
+                            Console.WriteLine($"{reader.GetName(i)}: {values[i]}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
