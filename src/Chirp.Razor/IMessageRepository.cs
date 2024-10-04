d386fa7c4c46a6a237655ddbf34bc909a139d7e3
polu <polu@itu.dk>
2024-10-04 12:49:28 +0200

    added ReadMessages

--- src/Chirp.Razor/IMessageRepository.cs
+++ src/Chirp.Razor/IMessageRepository.cs
@@ -3,4 +3,8 @@ namespace Chirp.Razor;
 public interface IMessageRepository
 {
     public Task CreateMessage(MessageDTO);
+
+    public Task<List<MessageDTO>> ReadMessages(string userName);
+    
+    
 }
