3912fd046f4d14da54113924c19ee37bb9127f29
polu <polu@itu.dk>
2024-10-04 17:37:02 +0200

    added Cheep class

---
+++ src/Chirp.Razor/Cheep.cs
@@ -0,0 +1,14 @@
+namespace Chirp.Razor;
+
+public class Cheep
+{
+    private string _text { get; set; }
+    private DateTime _dateTime { get; set; }
+    
+    public Cheep(string text, DateTime dateTime)
+    {
+        _text = text;
+        _dateTime = dateTime;
+    }
+    
+}
