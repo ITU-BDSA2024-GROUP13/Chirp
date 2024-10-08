e1d7cb9d1ce6593192d1e0872dc652a5f44bba32
polu <polu@itu.dk>
2024-10-08 12:25:09 +0200

    Added attributes and constructor to CheepDbContext. Co-authored by: Andreas <anln@itu.dk>

--- src/Chirp.Razor/CheepDBContext.cs
+++ src/Chirp.Razor/CheepDBContext.cs
@@ -4,5 +4,10 @@ namespace Chirp.Razor;
 
 public class CheepDBContext : DbContext
 {
-    
+    public DbSet<Author> authors { get; set; }
+    public DbSet<Cheep> cheeps { get; set; }
+
+    public CheepDBContext(DbContextOptions<CheepDBContext> options) : base(options)
+    {
+    }
 }
