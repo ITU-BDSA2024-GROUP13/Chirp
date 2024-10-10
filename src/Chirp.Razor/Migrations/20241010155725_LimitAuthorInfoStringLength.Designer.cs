dfc3a32c9a3bb659b4b7880d040b8e81002d71f5
polu <polu@itu.dk>
2024-10-10 17:58:42 +0200

    ran the migrations command. When I ran the command an error showed up that the chirps.db doesn't esxist. I assume this means we have to get the program to make one on build or something.

--- src/Chirp.Razor/Migrations/20241010155725_LimitAuthorInfoStringLength.Designer.cs
+++ src/Chirp.Razor/Migrations/20241010155725_LimitAuthorInfoStringLength.Designer.cs
@@ -2,6 +2,7 @@
 using Chirp.Razor.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Razor.Migrations
 {
     [DbContext(typeof(CheepDBContext))]
-    partial class CheepDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241010155725_LimitAuthorInfoStringLength")]
+    partial class LimitAuthorInfoStringLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
