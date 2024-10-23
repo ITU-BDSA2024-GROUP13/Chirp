cac184b543b8253c991d9ccb842defb6d066fa56
polu <polu@itu.dk>
2024-10-23 09:32:19 +0200

    added documentation to AuthorDTO

--- src/Chirp.Repositories/AuthorDTO.cs
+++ src/Chirp.Repositories/AuthorDTO.cs
@@ -1,5 +1,8 @@
 namespace Chirp.Repositories;
 
+/// <summary>
+/// This is a author data transfer object that is used to transfer author data between the client and the server.
+/// </summary>
 public class AuthorDTO
 {
    public int Id {get ; set; }
