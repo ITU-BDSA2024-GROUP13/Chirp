3f30bb28a40ac5e7a0b0dea172c57dd30ad3ad16
polu <polu@itu.dk>
2024-10-23 09:31:31 +0200

    added documentation to messageDTO

--- src/Chirp.Core/MessageDTO.cs
+++ src/Chirp.Core/MessageDTO.cs
@@ -1,5 +1,9 @@
 namespace Chirp.Core;
 
+/// <summary>
+/// This is a message data transfer object that is used to transfer message data between the client and the server.
+/// </summary>
+
 public class MessageDTO
 {
     public string username;
