1094d7a5f6ef0fa5e14d9218fcb28ce348fe7038
André Birk <arbi@itu.dk>
2024-10-25 16:21:31 +0200

    no further warnings

--- src/Chirp.Web/Razor/Public.cshtml.cs
+++ src/Chirp.Web/Razor/Public.cshtml.cs
@@ -1,29 +1,26 @@
-﻿namespace Chirp.Web;
+﻿namespace Chirp.Web.Razor;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Chirp.Services;
 using Chirp.Repositories;
 using System.Threading.Tasks;
+#pragma warning disable CS8604 // Possible null reference argument.
 
-public class PublicModel : PageModel
+public class PublicModel(ICheepService cheepService) : PageModel
 {
-    private readonly ICheepService _cheepService;
+    private readonly ICheepService _cheepService = cheepService;
 
-    public List<CheepDTO> Cheeps { get; set; }
-    public List<AuthorDTO> Authors { get; set; }
-    public int count {get; set; }
-    public int nextPage {get; set;}
-    public int previousPage {get; set;}
-    public int currentPage {get; set;}
+    public required List<CheepDTO> Cheeps { get; set; }
+    public required List<AuthorDTO> Authors { get; set; }
+    public int Count {get; set; }
+    public int NextPage {get; set;}
+    public int PreviousPage {get; set;}
+    public int CurrentPage {get; set;}
 
-    public int lastPage {get; set;}
-    public PublicModel(ICheepService cheepService)
-    {
-        _cheepService = cheepService;
-    }
+    public int LastPage {get; set;}
 
-    public int definePreviousPage(int page){
+    public int DefinePreviousPage(int page){
         if(page == 0){
             return 0;
         } else{
@@ -31,25 +28,25 @@ public class PublicModel : PageModel
         }
     }
 
-    public int defineLastPage(){
-        double p = count/32;
-        int lastPage = (int) Math.Ceiling(p);
-        return lastPage;
+    public int DefineLastPage(){
+        double p = Count/32;
+        int LastPage = (int) Math.Ceiling(p);
+        return LastPage;
     }
 
     public async Task<ActionResult> OnGetAsync(int page = 0)
     {
          var pageQuery = Request.Query["page"];
-        if (!pageQuery.Equals("") && pageQuery.Count() > 0){
+        if (!pageQuery.Equals("") && pageQuery.Count > 0){
             page = Int32.Parse(pageQuery[0]);
         }
-        currentPage = page;
-        nextPage = page+1;
-        previousPage = definePreviousPage(page);
+        CurrentPage = page;
+        NextPage = page+1;
+        PreviousPage = DefinePreviousPage(page);
         Cheeps = await _cheepService.ReadPublicMessages(page);
-        count = await _cheepService.CountPublicMessages();
+        Count = await _cheepService.CountPublicMessages();
         
-        lastPage = defineLastPage();
+        LastPage = DefineLastPage();
         return Page();
     }
     
--- src/Chirp.Web/Razor/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Razor/UserTimeline.cshtml.cs
@@ -1,4 +1,4 @@
-﻿namespace Chirp.Web;
+﻿namespace Chirp.Web.Razor;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,24 +6,22 @@ using Chirp.Services;
 using Chirp.Repositories;
 using System.Threading.Tasks;
 
-public class UserTimelineModel : PageModel
+#pragma warning disable CS8604 // Possible null reference argument.
+
+
+public class UserTimelineModel(ICheepService service) : PageModel
 {
-    private readonly ICheepService _service;
-    public List<CheepDTO> Cheeps { get; set; }
+    private readonly ICheepService _service = service;
+    public required List<CheepDTO> Cheeps { get; set; }
 
-    public int count { get; set; }
+    public int Count { get; set; }
     
-    public int nextPage {get; set;}
-    public int previousPage {get; set;}
-    public int currentPage {get; set;}
-    public int lastPage {get; set;}
+    public int NextPage {get; set;}
+    public int PreviousPage {get; set;}
+    public int CurrentPage {get; set;}
+    public int LastPage {get; set;}
 
-    public UserTimelineModel(ICheepService service)
-    {
-        _service = service;
-    }
-
-     public int definePreviousPage(int page){
+    public int DefinePreviousPage(int page){
         if(page == 0){
             return 0;
         } else{
@@ -31,8 +29,8 @@ public class UserTimelineModel : PageModel
         }
     }
 
-    public int defineLastPage(){
-        double p = count/32;
+    public int DefineLastPage(){
+        double p = Count/32;
         int lastPage = (int) Math.Ceiling(p);
         return lastPage;
     }
@@ -40,15 +38,15 @@ public class UserTimelineModel : PageModel
     public async Task<ActionResult> OnGetAsync(string author, int page = 0)
     {
         var pageQuery = Request.Query["page"];
-        if (!pageQuery.Equals("") && pageQuery.Count() > 0){
+        if (!pageQuery.Equals("") && pageQuery.Count > 0){
             page = Int32.Parse(pageQuery[0]);
         }
         Cheeps = await _service.ReadUserMessages(author, page);
-        count = await _service.CountUserMessages(author);
-        currentPage = page;
-        nextPage = page+1;
-        previousPage = definePreviousPage(page);
-        lastPage = defineLastPage();
+        Count = await _service.CountUserMessages(author);
+        CurrentPage = page;
+        NextPage = page+1;
+        PreviousPage = DefinePreviousPage(page);
+        LastPage = DefineLastPage();
         return Page();
     }
 }
