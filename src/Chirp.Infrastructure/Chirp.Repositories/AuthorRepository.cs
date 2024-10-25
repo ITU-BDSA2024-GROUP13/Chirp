5ac42243a670e0298b9ea9c333961216dd3dc1bc
Andreas Løvsgren Nielsen <75470603+AndreasLN@users.noreply.github.com>
2024-10-25 13:02:20 +0200

    find author by name

--- src/Chirp.Infrastructure/Chirp.Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/AuthorRepository.cs
@@ -7,13 +7,34 @@ namespace Chirp.Repositories;
 
 public class AuthorRepository : IAuhtorRepository {
 
+    private readonly CheepDBContext _dbContext;
+    public AuthorRepository(CheepDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
      public Task<int> CreateAuthor(CheepDTO newMessage){
         return null;
 
      }
 
-    public Task<List<AuthorDTO>> FindAuthorByName(string userName){
-        return null;
+    public async Task<List<AuthorDTO>> FindAuthorByName(string userName){
+        var query = _dbContext.Authors.OrderByDescending(author => author.Name)
+        .Where(author => author.Name.StartsWith(userName))
+        .Select(author => new AuthorDTO{ 
+            Id = author.AuthorId,
+            name = author.Name,
+            email = author.Email,
+            });
+        // Execute the query
+        var result = await query.ToListAsync();
+        
+        for (int i = 0; i < result.Count; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+
+        return result;
     }
 
     public Task<List<AuthorDTO>> FindAuthorByEmail(string email){
