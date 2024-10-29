b9ec0921d0cc817525b030045458c6c56d2fa678
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-10-29 16:23:58 +0100

    danishformat test

--- test/Infrastructure/Repositories/Repositories/HelperFunctionsTest.cs
+++ test/Infrastructure/Repositories/Repositories/HelperFunctionsTest.cs
@@ -13,4 +13,16 @@ public class HelperFunctionsTest
 
         Assert.Equal(unix, HelperFunctions.FromDateTimetoUnixTime(dateTime));
     }
+
+   [Fact]
+    public void danishFormat()
+    {
+        long unix = 1730213300000;
+
+        DateTime dateTime = HelperFunctions.FromUnixTimeToDateTime(unix);
+        string danishFormat = HelperFunctions.ReformatDateTimetoDanishFormat(dateTime);
+
+        Assert.Equal("29.10.2024 15.48.20", danishFormat);
+    }
+
 }
