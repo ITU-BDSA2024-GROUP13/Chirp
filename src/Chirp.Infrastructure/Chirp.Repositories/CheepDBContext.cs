31de5565d346b97bfb4e98bbe1342a9d566db9c4
Andreas Løvsgren Nielsen <anln@itu.dk>
2024-10-29 13:38:48 +0100

    changes in db context. Only accept cheeps with characters below 160

--- src/Chirp.Infrastructure/Chirp.Repositories/CheepDBContext.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/CheepDBContext.cs
@@ -10,11 +10,16 @@ public class CheepDBContext(DbContextOptions<CheepDBContext> options) : DbContex
     public DbSet<Author> Authors { get; set; }
     public DbSet<Cheep> Cheeps { get; set; }
 
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Cheep>()
+        .Property(t => t.Text)
+        .HasMaxLength(160);
 
         modelBuilder.Entity<Cheep>()
-        .HasKey(c => new { c.CheepId });
+        .HasKey(a => new { a.AuthorId });
+        
         modelBuilder.Entity<Author>()
         .HasKey(a => new { a.AuthorId });
     }
