01c26692c03cc5645970a053d9c0bef19e082599
alhhITU <alhh@itu.dk>
2024-11-03 13:48:55 +0100

    second commit because other things didn't get commited

---
+++ src/Chirp.Infrastructure/Chirp.Repositories/ApplicationUser.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Chirp.Repositories
+{
+public class ApplicationUser : IdentityUser { }
+}
--- src/Chirp.Infrastructure/Chirp.Repositories/Migrations/20241102204303_InitialCreate.Designer.cs
+++ src/Chirp.Infrastructure/Chirp.Repositories/Migrations/20241102204303_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Chirp.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Repositories.Migrations
 {
     [DbContext(typeof(CheepDBContext))]
-    partial class CheepDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241102204303_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
