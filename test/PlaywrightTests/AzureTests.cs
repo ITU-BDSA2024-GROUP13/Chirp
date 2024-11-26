434d94e2df709eba7d08a45190c6838408d52a68
alhhITU <alhh@itu.dk>
2024-11-26 15:43:51 +0100

    AzureTests now inherits from the IAsynzLifeTime interface which runs and initializes re-emerging objects (Iplaywright, Ibrowser, IbrowserContext).
    Co-Authored-By: https://www.danclarke.com/cleaner-tests-with-iasynclifetime

--- test/PlaywrightTests/AzureTests.cs
+++ test/PlaywrightTests/AzureTests.cs
@@ -1,35 +1,56 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow.DisposeAnalysis;
 using Microsoft.Playwright;
 using Xunit;
+using Xunit.Sdk;
 namespace PlaywrightTests;
-
-public class Azuretests
+public class AzureTests : IAsyncLifetime
 {
-    [Fact]
-    public async Task LogInTest()
+    private IPlaywright? playwright;
+    private IBrowser? browser;
+    private IBrowserContext? context;
+
+    public async Task DisposeAsync()
     {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        if(browser != null){ await browser.CloseAsync(); }
+        
+        if(playwright != null){ playwright.Dispose(); }
+    }
+
+    public async Task InitializeAsync()
+    {
+        playwright = await Playwright.CreateAsync();
+        browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
             Headless = true,
         });
-        var context = await browser.NewContextAsync();
 
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://bdsagroup013chirprazor.azurewebsites.net/?page=0");
-        string? ActualTitle = await page.Locator("h1").TextContentAsync();
-        string? ActualLogin = await page.Locator(".nav-item").TextContentAsync();
+        context = await browser.NewContextAsync();
+    }
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Logout Log in" }).ClickAsync();
-        await page.GetByPlaceholder("name@example.com").ClickAsync();
-        await page.GetByPlaceholder("name@example.com").FillAsync("Test@gmail.com");
-        await page.GetByPlaceholder("password").DblClickAsync();
-        await page.GetByPlaceholder("password").FillAsync("Chirp123!");
-        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+
+
+    [Fact]
+    public async Task LoginChangeTest()
+    {
         var page1 = await context.NewPageAsync();
-        await page1.GotoAsync("https://bdsagroup013chirprazor.azurewebsites.net/");
-        string? ActualLogout = await page1.Locator(".nav-item").TextContentAsync();
+        await page1.GotoAsync("https://bdsagroup013chirprazor.azurewebsites.net/?page=0");
+        string? ActualTitle = await page1.Locator("h1").TextContentAsync();
+        string? ActualLogin = await page1.Locator(".nav-item").TextContentAsync();
+
+
+        await page1.GetByRole(AriaRole.Link, new() { Name = "Logout Log in" }).ClickAsync();
+        await page1.GetByPlaceholder("name@example.com").ClickAsync();
+        await page1.GetByPlaceholder("name@example.com").FillAsync("Test@gmail.com");
+        await page1.GetByPlaceholder("password").DblClickAsync();
+        await page1.GetByPlaceholder("password").FillAsync("Chirp123!");
+        await page1.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        
+        var page2 = await context.NewPageAsync();
+        await page2.GotoAsync("https://bdsagroup013chirprazor.azurewebsites.net/");
+        string? ActualLogout = await page2.Locator(".nav-item").TextContentAsync();
 
         Assert.Equal("Chirp!", ActualTitle);
         Assert.Equal("Log in", ActualLogin.Trim());
