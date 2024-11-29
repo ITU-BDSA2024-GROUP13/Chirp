3c00245e10395e8e0e7e0dfd03374b77d3c21817
Andreas Løvsgren Nielsen <75470603+AndreasLN@users.noreply.github.com>
2024-11-29 12:15:39 +0100

    add dislikes in cheep model

--- src/Chirp.Web/models/CheepPartialView.cshtml.cs
+++ src/Chirp.Web/models/CheepPartialView.cshtml.cs
@@ -13,7 +13,9 @@ public class CheepPartialView()
     public required string Body { get; set; }
     public required bool isLiked { get; set; }
     public required long Date { get; set; }
-    public required int Like { get; set; }
+    public required int Likes { get; set; }
+    public required int Dislikes { get; set; }
+
     public DateTime ToDateTime(long value)
     {
         return Repositories.HelperFunctions.FromUnixTimeToDateTime(value);
