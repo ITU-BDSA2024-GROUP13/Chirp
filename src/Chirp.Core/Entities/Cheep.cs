f91b05ef275ea55a43a234ce11f8d8df1ac67d56
André <arbi@itu.dk>
2024-12-03 13:37:43 +0100

    added docu for core excluding DTO

--- src/Chirp.Core/Entities/Cheep.cs
+++ src/Chirp.Core/Entities/Cheep.cs
@@ -2,24 +2,55 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Chirp.Core.Entities;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-
 /// <summary>
-/// This is a cheep object which is used to store data about a cheep in a database.
+/// Represents a cheep (post) in the Chirp application.
+/// A cheep stores content, metadata, and user interactions.
 /// </summary>
 public class Cheep
 {
+    /// <summary>
+    /// Gets or sets the unique identifier for the cheep.
+    /// </summary>
     [Key]
     public int CheepId;
 
+    /// <summary>
+    /// Gets or sets the text content of the cheep.
+    /// The content is limited to 160 characters.
+    /// </summary>
     [Required]
     [StringLength(160)]
     public required string Text { get; set; }
-    public required DateTime TimeStamp { get; set; }
-    public required string AuthorId { get; set; }
-    public required List<Author> Likes { get; set; }
-    public required List<Author> Dislikes { get; set; }
-    public float LocalLikeRatio { get; set; } = 0;
-    public Author Author { get; set; }
 
+    /// <summary>
+    /// Gets or sets the timestamp indicating when the cheep was created.
+    /// </summary>
+    public required DateTime TimeStamp { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ID of the author who created the cheep.
+    /// </summary>
+    public required string AuthorId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of authors who liked the cheep.
+    /// </summary>
+    public required List<Author> Likes { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of authors who disliked the cheep.
+    /// </summary>
+    public required List<Author> Dislikes { get; set; }
+
+    /// <summary>
+    /// Gets or sets the local like ratio for the cheep.
+    /// This value is calculated based on likes and dislikes.
+    /// </summary>
+    public float LocalLikeRatio { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the author associated with the cheep.
+    /// Represents a navigation property for the author entity.
+    /// </summary>
+    public Author? Author { get; set; }
 }
